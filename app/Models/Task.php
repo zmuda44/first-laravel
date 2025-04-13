b02a9cd1f2c044fe34fdaa5df8e1d5f006ae6a5e
Greg Bailey <gregbailey617@gmail.com>
2025-04-12 23:45:09 -0400

    did something with models that you idnt upload

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -1,5 +1,7 @@
 <?php
 
+// this file was created by running this command. php artisan make:model Task -m (-m also makes a migration). in the main folder not app/models
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
