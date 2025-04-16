66e924fa887b0f305ca4cbcc64366f62cd15b361
Greg Bailey <gregbailey617@gmail.com>
2025-04-15 23:35:30 -0400

    got all tasks route working

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(2)->unverified()->create();
+
         \App\Models\Task::factory(20)->create();
 
         // \App\Models\User::factory()->create([
--- routes/web.php
+++ routes/web.php
@@ -46,44 +46,44 @@ class Task
   }
 }
 
-$tasks = [
-  new Task(
-    1,
-    'Buy groceries',
-    'Task 1 description',
-    'Task 1 long description',
-    false,
-    '2023-03-01 12:00:00',
-    '2023-03-01 12:00:00'
-  ),
-  new Task(
-    2,
-    'Sell old stuff',
-    'Task 2 description',
-    null,
-    false,
-    '2023-03-02 12:00:00',
-    '2023-03-02 12:00:00'
-  ),
-  new Task(
-    3,
-    'Learn programming',
-    'Task 3 description',
-    'Task 3 long description',
-    true,
-    '2023-03-03 12:00:00',
-    '2023-03-03 12:00:00'
-  ),
-  new Task(
-    4,
-    'Take dogs for a walk',
-    'Task 4 description',
-    null,
-    false,
-    '2023-03-04 12:00:00',
-    '2023-03-04 12:00:00'
-  ),
-];
+// $tasks = [
+//   new Task(
+//     1,
+//     'Buy groceries',
+//     'Task 1 description',
+//     'Task 1 long description',
+//     false,
+//     '2023-03-01 12:00:00',
+//     '2023-03-01 12:00:00'
+//   ),
+//   new Task(
+//     2,
+//     'Sell old stuff',
+//     'Task 2 description',
+//     null,
+//     false,
+//     '2023-03-02 12:00:00',
+//     '2023-03-02 12:00:00'
+//   ),
+//   new Task(
+//     3,
+//     'Learn programming',
+//     'Task 3 description',
+//     'Task 3 long description',
+//     true,
+//     '2023-03-03 12:00:00',
+//     '2023-03-03 12:00:00'
+//   ),
+//   new Task(
+//     4,
+//     'Take dogs for a walk',
+//     'Task 4 description',
+//     null,
+//     false,
+//     '2023-03-04 12:00:00',
+//     '2023-03-04 12:00:00'
+//   ),
+// ];
 
 $notasks = [''];
 
@@ -91,20 +91,29 @@ Route::get('/', function() {
   return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function () use($tasks) {
+// had ot comment out because you are no longer using the variable tasks Route::get('/tasks', function () use($tasks) {
+Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => $tasks
+        // 'tasks' => $tasks
+        'tasks' => \App\Models\Task::latest()->where('completed', true)->get()
     ]);
 })->name('tasks.index');
 
-Route::get('/tasks/{id}', function ($id) use($tasks) {
-  $task = collect($tasks)->firstWhere('id', $id);
+// commented out becaus eno longer using $tasks Route::get('/tasks/{id}', function ($id) use($tasks) {
+  Route::get('/tasks/{id}', function ($id) {
+  // $task = collect($tasks)->firstWhere('id', $id);
 
-  if (!$task) {
-    abort(Response::HTTP_NOT_FOUND);
-  }
+  // if (!$task) {
+  //   abort(Response::HTTP_NOT_FOUND);
+  // }
+  // got rid of these lines when we switched over to a db
 
-  return view('show', ['task' => $task]);
+  //can also import the Task and use a variable but the array above is a class
+
+  
+
+  // return view('show', ['task' => $task]);
+  return view('show', ['task' => \App\Models\Task::findOrFail($id)]);
   
 })->name('tasks.show');
 
